c14c0981ea1625327a32bc310b1d9890c97b01b6
Zemke <florian@zemke.io>
2020-03-30 20:04:53 +0200

    Yeah, we know game ID

--- waaas.py
+++ waaas.py
@@ -1,6 +1,15 @@
-print("It's going wild")
+import re
+
+res = {}
 
 with open("game.log", encoding="utf8", errors='ignore') as f:
   for line in f.readlines():
-    print(line)
+    if line.startswith("Game ID: "):
+
+      print(line)
+      res["gameId"] = re.compile("Game ID: \"(\d+)\"").search(line).group(1)
+
+
+      
+print(res)
 
