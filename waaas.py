f20873c2d18da9bb2a2c673fcfc64c597bc6988b
Zemke <florian@zemke.io>
2020-03-30 20:21:22 +0200

    startedAt

--- waaas.py
+++ waaas.py
@@ -5,9 +5,11 @@ res = {}
 with open("game.log", encoding="utf8", errors='ignore') as f:
   for line in f.readlines():
     if line.startswith("Game ID: "):
-
-      print(line)
       res["gameId"] = re.compile("Game ID: \"(\d+)\"").search(line).group(1)
+      continue
+    if line.startswith("Game Started at "):
+      res["startedAt"] = re.compile("Game Started at ([\d\- :]+ GMT)").search(line).group(1)
+      continue
 
 
       
