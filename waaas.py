dc52a9c4ac70a719bade28c8a16da48f42e85274
Zemke <florian@zemke.io>
2020-03-30 20:53:13 +0200

    Handle all the metadata

--- waaas.py
+++ waaas.py
@@ -2,16 +2,25 @@ import re
 
 res = {}
 
+
+def handle_action(line):
+  pass
+
+
 with open("game.log", encoding="utf8", errors='ignore') as f:
-  for line in f.readlines():
-    if line.startswith("Game ID: "):
-      res["gameId"] = re.compile("Game ID: \"(\d+)\"").search(line).group(1)
-      continue
-    if line.startswith("Game Started at "):
-      res["startedAt"] = re.compile("Game Started at ([\d\- :]+ GMT)").search(line).group(1)
-      continue
+  for l in f.readlines():
+    if re.search("^\[(?:\d\d:){2}\d\d\.\d\d\] ", l):
+      handle_action(l)
+    elif l.startswith("Game ID: "):
+      res["gameId"] = re.compile("Game ID: \"(\d+)\"").search(l).group(1)
+    elif l.startswith("Game Started at "):
+      res["startedAt"] = re.compile("Game Started at ([\d\- :]+ GMT)").search(l).group(1)
+    elif l.startswith("Game Engine Version: "):
+      res["engineVersion"] = re.compile("Game Engine Version: (.+)$").search(l).group(1)
+    elif l.startswith("Exported with Version: "):
+      res["exportVersion"] = re.compile("Exported with Version: (.+)$").search(l).group(1)
+    elif l.startswith("File Format Version: "):
+      file_format_groups = re.compile("File Format Version: (.+) - (.+)$").search(l)
+      res["fileFormatVersion"] = [file_format_groups.group(1), file_format_groups.group(2)]
 
-
-      
 print(res)
-
