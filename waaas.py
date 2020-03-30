714e424e8e5ff9ba727c7f8863933ea2816875cf
Zemke <florian@zemke.io>
2020-03-30 23:23:40 +0200

    Comment current unprocessed genera lines

--- waaas.py
+++ waaas.py
@@ -85,5 +85,23 @@ with open("game.log", encoding="ISO-8859-1", errors='ignore') as f:
     elif l.startswith("File Format Version: "):
       file_format_groups = re.compile("File Format Version: (.+) - (.+)$").search(l)
       res["fileFormatVersion"] = [file_format_groups.group(1), file_format_groups.group(2)]
+    else:
+      # l is not '\n' and print("Unprocessed", l)
+      # current unprocessed are
+      # Unprocessed Red:       "Siwy"      as "mloda kadra"
+      # Unprocessed Blue:      "NNN`Rafka" as "Men of faith" [Local Player]
+      # Unprocessed Spectator: "SiD`" [Host]
+      # Unprocessed Spectator: "TVPenguin"
+      # Unprocessed Spectator: "TheExtremist"
+      # Unprocessed Spectator: "waldek"
+      # Unprocessed Team time totals:
+      # Unprocessed mloda kadra (Siwy):       Turn: 00:09:02.62, Retreat: 00:00:59.70, Total: 00:10:02.32, Turn count: 25
+      # Unprocessed Men of faith (NNN`Rafka): Turn: 00:14:04.06, Retreat: 00:01:16.04, Total: 00:15:20.10, Turn count: 26
+      # Unprocessed End of round 3
+      # Unprocessed Round time: 0:34:37
+      # Unprocessed Total game time elapsed: 0:34:37
+      # Unprocessed Men of faith wins the round.
+      # Unprocessed Worm of the round: Michal (mloda kadra)
+      # Unprocessed Most damage with one shot: 259 - Adam (Men of faith)
 
 pprint.pprint(res)
