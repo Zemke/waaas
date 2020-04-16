4ddd1cddbed0c51fe228c5264194de0069ffde4b
Zemke <florian@zemke.io>
2020-04-16 22:35:24 +0200

    The structures themselves are listed next

--- land.py
+++ land.py
@@ -7,19 +7,16 @@ import bbb
 
 
 def perform(f):
-  """
+  res = {}
   res["signature"] = f.read(4)
   res["length"] = struct.unpack('i', f.read(4))
   res["width"] = struct.unpack('i', f.read(4))
   res["height"] = struct.unpack('i', f.read(4))
   res["cavernBorder"] = struct.unpack('????', f.read(4))
   res["waterHeight"] = struct.unpack('i', f.read(4))
-  # starting here things seem to go off from https://worms2d.info/Land_Data_file
   res["unknown"] = struct.unpack('i', f.read(4))
-  res["structuresX"] = struct.unpack('i', f.read(4))
-  res["structuresY"] = struct.unpack('i', f.read(4))
-  """
-  f.read(288)  # apparently 288 bytes till the img data
+  res["numOfStructures"] = struct.unpack('i', f.read(4))
+  f.read(res["numOfStructures"][0] * 2 * 4)
   return {"foreground": bbb.perform(f)}
 
 if __name__ == '__main__':
