1eb00993b69f49c78792c88588caec063f75c795
Zemke <florian@zemke.io>
2021-08-25 11:19:39 +0200

    Init AWS Lambda

--- README.md
+++ README.md
@@ -1,9 +1,84 @@
 # Worms Armageddon as a Service
 
-WAaaS is an API to [zemke/docker-wa](https://hub.docker.com/r/zemke/docker-wa) returning data from replay files in JSON format.
+WAaaS is frontend to [zemke/docker-wa](https://github.com/zemke/wa-docker)
+which allows for headless WA replay processing.
 
 ## Visualization example
 
 Screen shot from https://cwtsite.com/games/1602
 
 ![Data visualization example](https://user-images.githubusercontent.com/3391981/81668501-0e317d00-9445-11ea-9206-954444ffd97b.png)
+
+## Architecture on AWS
+
+```
+               o
+              /|\ <----------
+               |\            |
+                |            |
+                V            |
+             Upload          | JSON response
+           WAgame file       | Map file response
+                |            |
+                V            |
+          -----------------  |
+     ---->|   Lightsail   |--
+    |     -----------------\
+    |            |          \______________________
+    |         Upload                               |
+    |            |                                 |
+    |            V                                 |
+    |    ----------------------                    |
+    |    |   S3 waaas_input   |                    | Get map file
+    |    ----------------------                    |
+    |            |                                 |
+ Receive      Trigger                              |
+  JSON           |                                 |
+    |            V                                 V
+    |      --------------                     -----------------------
+    |      |   Lambda   |-- Upload map file ->|   S3 waaas_output   |
+    |      --------------                     -----------------------
+    |            |
+    |          Send
+    |            |
+    |            V
+    |       -----------
+     -------|   SQS   |
+            -----------
+```
+
+The S3 bucket should store the objects with a TTL.
+
+Another S3 (i.e. `waaas_static`) could provide the static files for the web frontend
+like for https://waaas.zemke.io.
+
+## Involved entities
+
+What's involved in this whole WAaaS thing?
+
+- wa-docker
+  - WA in a container with some convenience scripts for WA.exe's
+   [CLI options](https://worms2d.info/Command-line_options)
+  - There's an extension of it for running as a AWS Lambda
+   triggered by S3 object upload
+- AWS
+  - Lambda makes this parallelizable
+- Landing page
+  - Describe the thing (https://waaas.zemke.io)
+  - Form \*.WAgame file upload as an example
+  - **Future**: Form upload immediately providing a sample visualization<sup>1</sup>
+  - `cURL` example statement
+  - Reference implementation from CWT, WL and WR (if it were open source)
+- Python scripts
+  - Extracting map
+  - **Future**: Extract all kinds of things from replay<sup>2</sup>
+  - Parsing log to make a JSON format for further processing
+
+## Future
+
+<sup>1</sup> Extract Angular implementation from CWT into it's own Angular module.
+
+<sup>2</sup> S3 object has metadata which can be set during upload to
+[extract additional information from the replay](https://worms2d.info/Command-line_options). \
+Also, allow to toggle off map processing to save time when it's not needed.
+
---
+++ lambda/app.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import sys
+import boto3
+
+import re
+import json
+import os
+import time
+from tempfile import NamedTemporaryFile, TemporaryDirectory, gettempdir
+
+import waaas
+import land
+import bbb
+
+
+# {
+#   "Records": [
+#     {
+#       "eventVersion": "2.1",
+#       "eventSource": "aws:s3",
+#       "awsRegion": "us-east-2",
+#       "eventTime": "2019-09-03T19:37:27.192Z",
+#       "eventName": "ObjectCreated:Put",
+#       "userIdentity": {
+#         "principalId": "AWS:AIDAINPONIXQXHT3IKHL2"
+#       },
+#       "requestParameters": {
+#         "sourceIPAddress": "205.255.255.255"
+#       },
+#       "responseElements": {
+#         "x-amz-request-id": "D82B88E5F771F645",
+#         "x-amz-id-2": "vlR7PnpV2Ce81l0PRw6jlUpck7Jo5ZsQjryTjKlc5aLWGVHPZLj5NeC6qMa0emYBDXOo6QBU0Wo="
+#       },
+#       "s3": {
+#         "s3SchemaVersion": "1.0",
+#         "configurationId": "828aa6fc-f7b5-4305-8584-487c791949c1",
+#         "bucket": {
+#           "name": "lambda-artifacts-deafc19498e3f2df",
+#           "ownerIdentity": {
+#             "principalId": "A3I5XTEXAMAI3E"
+#           },
+#           "arn": "arn:aws:s3:::lambda-artifacts-deafc19498e3f2df"
+#         },
+#         "object": {
+#           "key": "b21b84d653bb07b05b1e6b33684dc11b",
+#           "size": 1305107,
+#           "eTag": "b21b84d653bb07b05b1e6b33684dc11b",
+#           "sequencer": "0C0F6F405D6ED209E1"
+#         }
+#       }
+#     }
+#   ]
+# }
+
+
+# TODO In WAaaS main repo split the logic from the web code so the logic can be reused here.
+
+
+def handler(event, context):
+  print("Received event: ", event)
+  print("Context: ", context)
+  s3 = boto3.resource('s3')
+  obj = s3.Object('waaas_input', event['Records'][0]["s3"]["object"]["key"])
+  with NamedTemporaryFile(mode='wb', prefix='waaas_', suffix="_replay") as replay_file:
+    obj.download_fileobj(replay_file)
+    handle(replay_file)
+
+
+def handle(replay_file):
+  with NamedTemporaryFile(mode='r+', prefix='waaas_', suffix="_log", encoding="ISO-8859-1") as log_file:
+    mapjson = None
+    texturejson = None
+    with TemporaryDirectory(prefix="waaas_", suffix="_land") as land_dir:
+      os.system('./perform ' + land_dir + ' ' +  replay_file.name + ' ' + log_file.name)
+      with open(land_dir + "/land.dat", mode='rb') as land_file:
+        with NamedTemporaryFile(mode='wb', prefix='waaas_', suffix="_map", delete=False) as map_file:
+          landres = land.perform(land_file)
+          bbb.toimage(landres["foreground"]).save(map_file, format='PNG')
+          texturejson = landres["texture"]
+          mapjson = "/map/" + re.compile("/waaas_(.+)_map").search(map_file.name).group(1)
+    if os.stat(log_file.name).st_size == 0:
+      raise Exception("could not process replay file")
+    logjson = waaas.perform(log_file)
+    logjson["map"] = mapjson
+    logjson["texture"] = texturejson
+    result = json.dumps(logjson)
+    print("result:\n", result)
+    return result
+    # TODO upload map file to S3 waaas-output
+    # TODO send JSON to SQS
+
+
+if __name__ == "__main__":
+  print("running from cli")
+  with NamedTemporaryFile(mode='wb', prefix='waaas_', suffix="_replay") as replay_file:
+    replay_file.write(inp['replay'])
+    handle(replay_file)
+
