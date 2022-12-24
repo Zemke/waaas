3e347b0e4b507e17323e031bab92f228bceda9eb
Zemke <florian@zemke.io>
2022-12-24 13:01:29 +0100

    Read object placements

--- land.py
+++ land.py
@@ -15,14 +15,20 @@ def perform(f):
   res["cavernBorder"] = struct.unpack('????', f.read(4))
   res["waterHeight"] = struct.unpack('i', f.read(4))
   res["unknown"] = struct.unpack('i', f.read(4))
-  res["numOfStructures"] = struct.unpack('i', f.read(4))
-  f.read(res["numOfStructures"][0] * 2 * 4)
+  res["objectPlacements"] = {
+    "quantity": struct.unpack('i', f.read(4)),
+  }
+  pl = []
+  for _ in range(res["numOfStructures"][0]):
+    pl.append((struct.unpack('i', f.read(4))[0], struct.unpack('i', f.read(4))[0]))
+  res["objectPlacements"]["coords"] = pl
   res["foreground"] = bbb.perform(f)
   res["monochrome"] = bbb.perform(f)
   res["background"] = bbb.perform(f)
   res["textureLength"] = int.from_bytes(f.read(1),byteorder='big')
   res["texture"] = struct.unpack(str(res["textureLength"]) + 's', f.read(res["textureLength"]))[0].decode()
   return res
+
 
 if __name__ == '__main__':
   with open(sys.argv[1], "rb") as f:
